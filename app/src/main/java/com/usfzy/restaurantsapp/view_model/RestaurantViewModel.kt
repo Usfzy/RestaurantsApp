c87a45307bbce9e8269c1dc566fda23c30f9bdb7
Usfzy <usfzy47@gmail.com>
2024-01-24 20:15:18 +0000

    changed to default coroutine scope

--- app/src/main/java/com/usfzy/restaurantsapp/view_model/RestaurantViewModel.kt
+++ app/src/main/java/com/usfzy/restaurantsapp/view_model/RestaurantViewModel.kt
@@ -3,6 +3,7 @@ package com.usfzy.restaurantsapp.view_model
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.usfzy.restaurantsapp.model.Restaurant
 import com.usfzy.restaurantsapp.retrofit.RestaurantsApiService
 import kotlinx.coroutines.CoroutineScope
@@ -16,9 +17,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RestaurantViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
     val state = mutableStateOf(emptyList<Restaurant>())
     private var restInterface: RestaurantsApiService
-
-    private val job = Job()
-    private val scope = CoroutineScope(context = job + Dispatchers.IO)
 
     init {
         val retrofit: Retrofit = Retrofit
@@ -34,9 +32,9 @@ class RestaurantViewModel(private val stateHandle: SavedStateHandle) : ViewModel
 
     private fun getRestaurants() {
 
-        scope.launch {// LAUNCH COROUTINE ON IO THREAD
+        viewModelScope.launch(Dispatchers.IO) {// LAUNCH COROUTINE ON IO THREAD
             val restaurants = restInterface.getRestaurants()
-            
+
             withContext(Dispatchers.Main) {
                 state.value = restaurants
             } // UPDATE UI ON MAIN THREAD
@@ -82,9 +80,4 @@ class RestaurantViewModel(private val stateHandle: SavedStateHandle) : ViewModel
         return this
     }
 
-    override fun onCleared() {
-        super.onCleared()
-
-        job.cancel()
-    }
 }
